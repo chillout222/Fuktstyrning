41de97c29dc7a89a6484a914930fcc9e23d1e102
Chillout222 <hermansson.lars@gmail.com>
2025-04-17 13:09:50 +0200

    Modified config_flow.py

--- custom_components/fuktstyrning/config_flow.py
+++ custom_components/fuktstyrning/config_flow.py
@@ -40,40 +40,109 @@ class FuktstyrningConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 hass = self.hass
                 
                 humidity_sensor = user_input.get(CONF_HUMIDITY_SENSOR)
-                if humidity_sensor and not hass.states.get(humidity_sensor):
-                    errors[CONF_HUMIDITY_SENSOR] = "entity_not_found"
+                if humidity_sensor:
+                    humidity_sensor = humidity_sensor.strip()
+                    _LOGGER.debug("Validating humidity sensor: %s", humidity_sensor)
+                    if not hass.states.get(humidity_sensor):
+                        # Extra kontroll - kolla om det finns i listan av alla entities
+                        all_entities = list(hass.states.async_entity_ids())
+                        if humidity_sensor in all_entities:
+                            _LOGGER.debug("Entity finns i listan men inte i states - fortsätter ändå")
+                        else:
+                            _LOGGER.warning("Entity %s hittades inte i HA", humidity_sensor)
+                            errors[CONF_HUMIDITY_SENSOR] = "entity_not_found"
                     
                 price_sensor = user_input.get(CONF_PRICE_SENSOR)
-                if price_sensor and not hass.states.get(price_sensor):
-                    errors[CONF_PRICE_SENSOR] = "entity_not_found"
+                if price_sensor:
+                    price_sensor = price_sensor.strip()
+                    _LOGGER.debug("Validating price sensor: %s", price_sensor)
+                    if not hass.states.get(price_sensor):
+                        # Extra kontroll - kolla om det finns i listan av alla entities
+                        all_entities = list(hass.states.async_entity_ids())
+                        if price_sensor in all_entities:
+                            _LOGGER.debug("Entity finns i listan men inte i states - fortsätter ändå")
+                        else:
+                            _LOGGER.warning("Entity %s hittades inte i HA", price_sensor)
+                            errors[CONF_PRICE_SENSOR] = "entity_not_found"
                     
                 dehumidifier_switch = user_input.get(CONF_DEHUMIDIFIER_SWITCH)
-                if dehumidifier_switch and not hass.states.get(dehumidifier_switch):
-                    errors[CONF_DEHUMIDIFIER_SWITCH] = "entity_not_found"
+                if dehumidifier_switch:
+                    dehumidifier_switch = dehumidifier_switch.strip()
+                    _LOGGER.debug("Validating dehumidifier switch: %s", dehumidifier_switch)
+                    if not hass.states.get(dehumidifier_switch):
+                        # Extra kontroll - kolla om det finns i listan av alla entities
+                        all_entities = list(hass.states.async_entity_ids())
+                        if dehumidifier_switch in all_entities:
+                            _LOGGER.debug("Entity finns i listan men inte i states - fortsätter ändå")
+                        else:
+                            _LOGGER.warning("Entity %s hittades inte i HA", dehumidifier_switch)
+                            errors[CONF_DEHUMIDIFIER_SWITCH] = "entity_not_found"
                     
                 weather_entity = user_input.get(CONF_WEATHER_ENTITY)
-                if weather_entity and not hass.states.get(weather_entity):
-                    errors[CONF_WEATHER_ENTITY] = "entity_not_found"
+                if weather_entity:
+                    weather_entity = weather_entity.strip()
+                    if not hass.states.get(weather_entity):
+                        # Extra kontroll - kolla om det finns i listan av alla entities
+                        all_entities = list(hass.states.async_entity_ids())
+                        if weather_entity in all_entities:
+                            _LOGGER.debug("Entity finns i listan men inte i states - fortsätter ändå")
+                        else:
+                            errors[CONF_WEATHER_ENTITY] = "entity_not_found"
                 
                 outdoor_humidity_sensor = user_input.get(CONF_OUTDOOR_HUMIDITY_SENSOR)
-                if outdoor_humidity_sensor and not hass.states.get(outdoor_humidity_sensor):
-                    errors[CONF_OUTDOOR_HUMIDITY_SENSOR] = "entity_not_found"
+                if outdoor_humidity_sensor:
+                    outdoor_humidity_sensor = outdoor_humidity_sensor.strip()
+                    if not hass.states.get(outdoor_humidity_sensor):
+                        # Extra kontroll - kolla om det finns i listan av alla entities
+                        all_entities = list(hass.states.async_entity_ids())
+                        if outdoor_humidity_sensor in all_entities:
+                            _LOGGER.debug("Entity finns i listan men inte i states - fortsätter ändå")
+                        else:
+                            errors[CONF_OUTDOOR_HUMIDITY_SENSOR] = "entity_not_found"
                     
                 outdoor_temp_sensor = user_input.get(CONF_OUTDOOR_TEMP_SENSOR)
-                if outdoor_temp_sensor and not hass.states.get(outdoor_temp_sensor):
-                    errors[CONF_OUTDOOR_TEMP_SENSOR] = "entity_not_found"
+                if outdoor_temp_sensor:
+                    outdoor_temp_sensor = outdoor_temp_sensor.strip()
+                    if not hass.states.get(outdoor_temp_sensor):
+                        # Extra kontroll - kolla om det finns i listan av alla entities
+                        all_entities = list(hass.states.async_entity_ids())
+                        if outdoor_temp_sensor in all_entities:
+                            _LOGGER.debug("Entity finns i listan men inte i states - fortsätter ändå")
+                        else:
+                            errors[CONF_OUTDOOR_TEMP_SENSOR] = "entity_not_found"
                     
                 power_sensor = user_input.get(CONF_POWER_SENSOR)
-                if power_sensor and not hass.states.get(power_sensor):
-                    errors[CONF_POWER_SENSOR] = "entity_not_found"
+                if power_sensor:
+                    power_sensor = power_sensor.strip()
+                    if not hass.states.get(power_sensor):
+                        # Extra kontroll - kolla om det finns i listan av alla entities
+                        all_entities = list(hass.states.async_entity_ids())
+                        if power_sensor in all_entities:
+                            _LOGGER.debug("Entity finns i listan men inte i states - fortsätter ändå")
+                        else:
+                            errors[CONF_POWER_SENSOR] = "entity_not_found"
                     
                 energy_sensor = user_input.get(CONF_ENERGY_SENSOR)
-                if energy_sensor and not hass.states.get(energy_sensor):
-                    errors[CONF_ENERGY_SENSOR] = "entity_not_found"
+                if energy_sensor:
+                    energy_sensor = energy_sensor.strip()
+                    if not hass.states.get(energy_sensor):
+                        # Extra kontroll - kolla om det finns i listan av alla entities
+                        all_entities = list(hass.states.async_entity_ids())
+                        if energy_sensor in all_entities:
+                            _LOGGER.debug("Entity finns i listan men inte i states - fortsätter ändå")
+                        else:
+                            errors[CONF_ENERGY_SENSOR] = "entity_not_found"
                     
                 voltage_sensor = user_input.get(CONF_VOLTAGE_SENSOR)
-                if voltage_sensor and not hass.states.get(voltage_sensor):
-                    errors[CONF_VOLTAGE_SENSOR] = "entity_not_found"
+                if voltage_sensor:
+                    voltage_sensor = voltage_sensor.strip()
+                    if not hass.states.get(voltage_sensor):
+                        # Extra kontroll - kolla om det finns i listan av alla entities
+                        all_entities = list(hass.states.async_entity_ids())
+                        if voltage_sensor in all_entities:
+                            _LOGGER.debug("Entity finns i listan men inte i states - fortsätter ändå")
+                        else:
+                            errors[CONF_VOLTAGE_SENSOR] = "entity_not_found"
                 
                 if not errors:
                     # Create entry
